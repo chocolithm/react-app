92e890d9d3f2ea4dc3d4ae23a4f2c0e56745a6db
chocolithm <kys8336@gmail.com>
2025-02-13 15:30:34 +0900

    BrouseRouter, HashRouter, Link, NavLink

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
+import { HashRouter, Route, Routes, NavLink } from 'react-router-dom';
 
 function Home() {
   return (
@@ -34,14 +35,22 @@ function App() {
   return (
     <div>
       <h1>Hello React Router Dom</h1>
-      <Home />
-      <Topics />
-      <Contact />
+      <ul>
+        <li><NavLink to="/">Home</NavLink></li>
+        <li><NavLink to="/topics">Topics</NavLink></li>
+        <li><NavLink to="/contact">Contact</NavLink></li>
+      </ul>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/topics" element={<Topics />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/*" element={'Not Found'} />
+      </Routes>
     </div>
   );
 }
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />);
+ReactDOM.createRoot(document.getElementById('root')).render(<HashRouter><App /></HashRouter>);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
